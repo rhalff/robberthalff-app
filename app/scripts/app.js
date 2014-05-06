cd3147cf5b045a61255bbcc620bab7dc0da8d9e1
Rob Halff <rob.halff@gmail.com>
2014-05-07 01:38:39 +0200

    yo angular robberthalff

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular
+  .module('robberthalffApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
